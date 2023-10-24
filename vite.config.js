473132ef634c6b216a2cdcaea0bff0d8a7c2862a
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2023-10-24 15:59:30 +0900

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
+import replace from '@rollup/plugin-replace';
+
+dotenv.config();
+
+export default defineConfig({
+	// Your VITE configuration settings here
+	server: {
+		host: true,
+	},
+
+	// Change the output format to ESM
+	build: {
+		rollupOptions: {
+			output: {
+				format: 'esm', // Set the format to 'esm'
+			},
+			plugins: [
+				replace({
+					'process.env': JSON.stringify(import.meta.env),
+					preventAssignment: true, // Set preventAssignment to true
+
+				}),
+			],
+		},
+	},
+});
